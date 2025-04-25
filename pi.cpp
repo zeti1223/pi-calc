2dceb759ffcae7025a9bb70a2f5b13337cf5b506
Zeti_1223 <121749710+zeti1223@users.noreply.github.com>
2025-04-25 15:59:32 +0200

    Update pi.cpp

--- pi.cpp
+++ pi.cpp
@@ -4,32 +4,25 @@
 #include <chrono>
 
 int main() {
-    for (int run = 1; run <= 20; ++run) {
-        std::cout << "Futás #" << run << std::endl;
-        auto start_time = std::chrono::high_resolution_clock::now();
-
-        long long k = 1;
-        double s = 0.0;
-        const long long iterations = 10000000000;
-        std::cout << std::fixed << std::setprecision(15);
-
-        for (long long i = 0; i < iterations; ++i) {
-            if (i % 2 == 0) {
-                s += 4.0 / k;
-            } else {
-                s -= 4.0 / k;
-            }
-            k += 2;
+    auto start_time = std::chrono::high_resolution_clock::now();
+    long long k = 1;
+    double s = 0.0;
+    const long long iterations = 10000000000;
+    std::cout << std::fixed << std::setprecision(15);
+    for (long long i = 0; i < iterations; ++i) {
+        if (i % 2 == 0) {
+            s += 4.0 / k;
+        } else {
+            s -= 4.0 / k;
         }
-
-        std::cout << s << std::endl;
-        std::cout << (M_PI - s) << std::endl;
-
-        auto end_time = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed = end_time - start_time;
-        std::cout << "A kód futási ideje: " << std::fixed << std::setprecision(5)
-                  << elapsed.count() << " másodperc" << std::endl << std::endl;
-    }
+        k += 2;
+       }
+    std::cout << s << std::endl;
+    std::cout << (M_PI - s) << std::endl;
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end_time - start_time;
+    std::cout << "A kód futási ideje: " << std::fixed << std::setprecision(5)
+              << elapsed.count() << " másodperc" << std::endl;
 
     return 0;
 }
